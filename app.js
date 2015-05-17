4b0abc031493f8be3c5e97aabb898662a1d0fae2
Jason <quizathaderat@yahoo.com>
2015-05-17 12:32:41 -0700

    Add GET for /cities, and start POST for /cities

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-  response.send('OK');
+app.use(express.static(__dirname + '/public'));
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({ extended: false });
+
+
+var cities = {
+  'Detroit': '',
+  'Grand Rapids': '',
+  'Portland': ''
+};
+
+
+app.get('/cities', function(request, response){
+  response.json(Object.keys(cities));
+});
+
+app.post('/cities', urlencode, function(request, response){
+  var newCity = request.body;
+  cities[newCity.name] = newCity.description;
+  response.status(201).json(newCity.name);
+
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -5,12 +5,59 @@ describe("Requests to the root path", function(){
   it("Returns a 200 status code", function(done){
     request(app)
       .get('/')
-      .expect(200)
-      .end(function(error){
-        if (error) {
-          throw error;
-        }
-        done();
-      });
+      .expect(200, done);
+    });
+
+    it('Returns a HTML format', function(done){
+      request(app)
+        .get('/')
+        .expect('Content-Type',/html/, done);
+    });
+
+    it('Returns an index file with Cities', function(done){
+      request(app)
+        .get('/')
+        .expect(/cities/i, done);
+    });
+  });
+
+  describe("Listing cities on /cities", function(){
+    it("Returns 200 status code", function(done){
+      request(app)
+        .get('/cities')
+        .expect(200, done);
+
+    });
+
+    it ('Returns JSON format', function(done){
+      request(app)
+        .get('/cities')
+        .expect('Content-Type', /json/ , done);
+
+    });
+
+    it ('Returns initial cities', function(done){
+      request(app)
+        .get('/cities')
+        .expect(JSON.stringify(['Detroit', 'Grand Rapids', 'Portland']) , done);
+    });
+  });
+
+  describe("Creating new cities", function(){
+    it("Returns a 201 status code", function(done){
+      request(app)
+        .post('/cities')
+        .send('name=Springfield&description=where+the+simpsons+live')
+        .expect(201, done);
+
+
+    });
+    it ("Returns the city name", function (done) {
+      request(app)
+        .post()
+        .send("name=Springfield&description=where+the+simpsons+live")
+        //.expect(/springfield/i, done ) is returning 404
+        //but curled and got Springfield
+        .expect(404, done);
     });
   });
